bc451ffa09d311171afb2179161f23ccceb547ad
amitkinor <amit.kinor@gmail.com>
2020-08-02 17:03:27 +0300

    added lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,41 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
-import './Blog.css';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink
-                                to="/posts/"
-                                exact
-                                activeClassName="my-active"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Posts</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                <Switch>
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/posts" component={Posts} />
-                </Switch>
-            </div>
-        );
-    }
+  state = {
+    auth: true,
+  };
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          {this.state.auth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
+          <Route path="/posts" component={Posts} />
+
+          <Route render={() => <h1>Not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
